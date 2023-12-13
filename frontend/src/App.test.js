0a4035420d72dadb6fade78b1aacf49573e6b81d
karluga <kaarlisbrakis@gmail.com>
2023-12-13 16:47:49 +0200

    Made mock tests for PHP backend.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,52 +1,83 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import App from './App'; // assuming this is the main component of your app
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import axios from 'axios';  // Import Axios
+import RouteReport from './pages/RouteReport.js';
 
-const mockAxios = new MockAdapter(axios);
+// Mock Axios
+jest.mock('axios');
 
-
-// Test to check if the main page renders without crashing
-test('renders main page without crashing', () => {
-  render(<App />);
-
-  // Check if the image with alt text "logo" is present
-  const logoImage = screen.getByAltText('logo');
-  expect(logoImage).toBeInTheDocument();
-});
-
-// Mocking the successful registration response
-mockAxios.onPost('http://localhost/backend/register.php').reply(200, {
-  user: {
-    id: 123, // replace with the actual value of $lastInsertId
-    username: 'jesttest', // replace with the actual value of $username
+// Sample response data
+const mockResponseData = {
+  data: {
+    error: null,
+    data: {
+      units: [
+        {
+          unit_id: 'sample_unit_id',
+          routes: [
+            {
+              route_id: 'sample_route_id',
+              type: 'route',
+              start: { time: '2023-01-01T00:00:00Z', address: 'Start Address' },
+              end: { time: '2023-01-01T01:00:00Z', address: 'End Address' },
+              avg_speed: 60,
+              max_speed: 80,
+              distance: 10000,
+              decoded_route: 'sample_decoded_route',
+            },
+          ],
+        },
+      ],
+    },
   },
-});
+};
 
-test('simulates user registration', async () => {
-  render(<App />);
+// Sample vehicle options
+const mockVehicleOptions = [
+  { unit_id: '81297', number: 'JL-2400', label: 'BMW' },
+];
 
-  // Navigate to the registration form or component
-  // You need to implement a way to navigate to the registration page or component
+describe('RouteReport', () => {
+  // Mocking useEffect to avoid issues with useJsApiLoader
+  jest.spyOn(React, 'useEffect').mockImplementation((f) => f());
 
-  // Simulate user input
-  userEvent.type(screen.getByLabelText(/registerUsername/i), 'jesttest');
-  userEvent.type(screen.getByLabelText(/registerPassword/i), 'jesttest');
-  userEvent.type(screen.getByLabelText(/registerEmail/i), 'jest@test.com');
+  it('renders RouteReport component and fetches data', async () => {
+    // Mocking the Axios get method to return the sample data
+    axios.get.mockResolvedValueOnce(mockResponseData);
 
-  // Simulate form submission
-  userEvent.click(screen.getByRole('button', { name: /register/i }));
+    render(<RouteReport />);
 
-  // Wait for the registration process to complete
-  await waitFor(() => {
-    // Assert that the registration was successful
-    expect(screen.getByText(/Logged in as jesttest/i)).toBeInTheDocument();
+    // Select a vehicle
+    fireEvent.change(screen.getByLabelText(/Vehicle number/i), {
+      target: { value: '81297' },
+    });
+
+    // Set date range
+    fireEvent.change(screen.getByLabelText(/From/i), {
+      target: { value: '2023-01-01' },
+    });
+    fireEvent.change(screen.getByLabelText(/To/i), {
+      target: { value: '2023-01-02' },
+    });
+
+    // Click the generate button
+    fireEvent.click(screen.getByText(/Generate/i));
+
+    // Wait for the Axios request to be completed
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
+
+    // Check if the component renders the fetched route data
+    expect(screen.getByText(/Start Address/i)).toBeInTheDocument();
+    expect(screen.getByText(/End Address/i)).toBeInTheDocument();
+    // Add more assertions based on your actual component content
+
+    // Mocking XLSX.writeFile to avoid actual file creation during testing
+    jest.spyOn(window.XLSX, 'writeFile').mockImplementation(() => {});
+
+    // Click the export to Excel button
+    fireEvent.click(screen.getByText(/Export to Excel/i));
+
+    // Check if XLSX.writeFile is called
+    expect(window.XLSX.writeFile).toHaveBeenCalledTimes(1);
   });
-});
-
-// Clean up mock after all tests are done
-afterAll(() => {
-  mockAxios.restore();
 });
